063860eeeb35462d10c52df28c17f761da6fa36f
The-Orgin <dhanuv48@gmail.com>
2026-03-24 23:31:02 +0530

    UC1

---
+++ src/main/java/HelloApp.java
@@ -0,0 +1,12 @@
+/*
+*HelloApp UC1
+*
+*@author Dhanush
+*@version 1.0 
+*/
+
+public class HelloApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
