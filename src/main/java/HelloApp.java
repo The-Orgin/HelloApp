27e31042385e21eadac2225879aa5e07a1c7541e
The-Orgin <dhanuv48@gmail.com>
2026-03-30 10:48:23 +0530

    UC6

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,25 +1,28 @@
 /*
-*HelloApp UC5
+*HelloApp UC6
 *
 *@author Dhanush
-*@version 5.0 
+*@version 6.0 
 */
 
 public class HelloApp {
     public static void main(String[] args) {
-        String name = "World";
-
-        StringBuilder nameBuilder = new StringBuilder();
-        boolean first = true;
-        for (String arg : args) {
-            if(!first){
-                nameBuilder.append(", ");
-            }
-            nameBuilder.append(arg);
-            first = false;
-            name = nameBuilder.toString();
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
         }
 
-        System.out.println("Hello, " + name + "!");
+        StringBuilder nameBuilder = new StringBuilder();
+        
+        for (String arg : args) {
+            nameBuilder.append(arg).append(", ");
+        }
+
+        String names = "";
+        if (nameBuilder.length() > 0) {
+            names = nameBuilder.substring(0, nameBuilder.length() - 2);
+        }
+
+        System.out.println("Hello, " + names + "!");
     }
 }
