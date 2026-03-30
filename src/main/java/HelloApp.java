44ce4c63590400e8785d57b9e5cbccf6c9133ad0
The-Orgin <dhanuv48@gmail.com>
2026-03-30 14:44:12 +0530

    UC7

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,28 +1,17 @@
 /*
-*HelloApp UC6
+*HelloApp UC7
 *
 *@author Dhanush
-*@version 6.0 
+*@version 7.0 
 */
 
 public class HelloApp {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-            return;
+        String names = "World";
+        if (args.length > 0) {
+            names = String.join(", ", args);
         }
-
-        StringBuilder nameBuilder = new StringBuilder();
         
-        for (String arg : args) {
-            nameBuilder.append(arg).append(", ");
-        }
-
-        String names = "";
-        if (nameBuilder.length() > 0) {
-            names = nameBuilder.substring(0, nameBuilder.length() - 2);
-        }
-
         System.out.println("Hello, " + names + "!");
     }
 }
